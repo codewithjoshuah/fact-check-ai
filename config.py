0667a07bef69a3e66dc59cbfeb77ae7d210998c5
codewithjoshuah <joshuahrgnanraj@gmail.com>
2026-07-19 15:53:02 +0530

    Secure API key using environment variables

--- config.py
+++ config.py
@@ -4,14 +4,3 @@ import os
 load_dotenv()
 
 TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
-
-TRUSTED_DOMAINS = [
-    "reuters.com",
-    "apnews.com",
-    "bbc.com",
-    "nasa.gov",
-    "who.int",
-    "cdc.gov",
-    "nature.com",
-    "science.org"
-]
